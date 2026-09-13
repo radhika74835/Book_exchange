10e97a124c49ce170a5cddb7263e6376eebdf1d2
radhika74835 <radhika636027@gmail.com>
2026-09-13 14:35:49 +0530

    update admin_dashboard.php

--- admin_books.php
+++ admin_books.php
@@ -3,9 +3,6 @@
 session_start();
 include("db_connect.php");
 
-
-/* Admin check */
-
 if (!isset($_SESSION["student_id"]) ||
     !isset($_SESSION["is_admin"]) ||
     $_SESSION["is_admin"] != 1) {
@@ -13,9 +10,6 @@ if (!isset($_SESSION["student_id"]) ||
     header("Location: admin_login.php");
     exit();
 }
-
-
-/* Get books */
 
 $query = mysqli_query(
     $conn,
